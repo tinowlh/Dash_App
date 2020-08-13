9a6931727ad646621b0659449ab8c6827e00fb12
tinowlh <tinowhuang@gmail.com>
2020-08-13 18:12:09 +0800

    add kmeans clustering

--- stock_app.py
+++ stock_app.py
@@ -15,6 +15,12 @@ import plotly.express as px
 import plotly.graph_objects as go
 import plotly.io as pio #themes
 
+from sklearn.cluster import KMeans
+
+# testttt
+
+
+
 #import datetime
 #import investpy
 
@@ -50,14 +56,20 @@ import plotly.io as pio #themes
 #                                    to_date= today)
 
 
+def get_stockP_raw(l_of_stocks, start = dt(2020, 1, 1), end = dt.now()):
+    # data preparation
+    df = web.DataReader(l_of_stocks, 'yahoo', start, end)
+    df = df.loc[:, df.columns.get_level_values(0).isin({'Close'})]
+    df.columns =df.columns.droplevel()
+    return df
+
 
 def get_stockP(l_of_stocks, start = dt(2020, 1, 1), end = dt.now()):
-    df_stock = web.DataReader(l_of_stocks, 'yahoo', start, end)
-    df_stock = df_stock.loc[:, df_stock.columns.get_level_values(0).isin({'Close'})]
-    df_stock.columns =df_stock.columns.droplevel()
+    df_stock = get_stockP_raw(l_of_stocks, start, end)
     df_stock = df_stock.reset_index('Date').round(2)
     df_stock['Date'] = df_stock['Date'].dt.date
     return df_stock
+
 
 def get_stockP_return(df_stock):
     df_stock = df_stock.set_index('Date')
@@ -67,9 +79,7 @@ def get_stockP_return(df_stock):
 
 
 def get_cum_return(l_of_stocks, start = dt(2020, 1, 1), end = dt.now()):
-    df = web.DataReader(l_of_stocks, 'yahoo', start, end)
-    df = df.loc[:, df.columns.get_level_values(0).isin({'Close'})]
-    df.columns =df.columns.droplevel()
+    df = get_stockP_raw(l_of_stocks, start, end)
     df_daily_return = df.pct_change()
     df_daily_cum_return = (1 + df_daily_return).cumprod()
     df_daily_cum_return = df_daily_cum_return.reset_index('Date')
@@ -79,6 +89,33 @@ def get_cum_return(l_of_stocks, start = dt(2020, 1, 1), end = dt.now()):
                          value_name='CumReturn')
     return df_unpivot
 
+
+def get_annualizedReturn(l_of_stocks, start = dt(2020, 1, 1), end = dt.now()):
+    df = get_stockP_raw(l_of_stocks, start, end)
+    ttl_return = (df.iloc[-1] - df.iloc[0]) / df.iloc[0]
+    df_ttl_return = ttl_return.to_frame(name="TTLReturn")
+    df_annualized_return = ((1 + df_ttl_return) ** (365 / len(df))) -1 
+    df_annualized_return = df_annualized_return.rename(columns={"TTLReturn": "AnnReturn"})
+    return df_annualized_return
+   
+def get_volatility(l_of_stocks, start = dt(2020, 1, 1), end = dt.now()):
+    df = get_stockP_raw(l_of_stocks, start, end)
+    df_return = df.pct_change()
+    sr_volatility = df_return.std() * np.sqrt(250)
+    df_volatility = sr_volatility.to_frame(name="Volatility")
+    return df_volatility
+
+def get_df_cluster(l_of_stocks, start = dt(2020, 1, 1), end = dt.now()):
+    df_annualized_return = get_annualizedReturn(l_of_stocks, start, end)
+    df_volatility = get_volatility(l_of_stocks, start, end)
+    
+    # merge
+    df_cluster = pd.merge(df_annualized_return, 
+                          df_volatility, 
+                          left_index=True, right_index = True)
+    df_cluster = df_cluster.reset_index()
+    
+    return df_cluster
 
 
 
@@ -95,6 +132,8 @@ df_stockls = pd.read_csv('stocklist.csv')
 df_stockls = df_stockls[~df_stockls['value'].isin(['NVDA', 'BND'])]
 # df for table
 l_of_stocks = df_stockls['value'].tolist()
+l_cluster = df_stockls['value'].tolist()
+l_cluster.remove('0050.TW')
 df_stock = get_stockP(l_of_stocks)
 #df_stock_return = get_stockP_return(df_stock)
 
@@ -129,6 +168,7 @@ TEXT_STYLE = {
 # sidebar controls
 controls = dbc.FormGroup(
     [
+     
         dcc.Markdown('Stock Symbol'),
         dcc.Dropdown(
             id='my-dropdown',
@@ -163,7 +203,10 @@ controls = dbc.FormGroup(
             options= df_stockls[df_stockls['benchmark'] == 'N'][['value', 'label']].to_dict('records'),
             value='0050.TW',
             style= {'color':'#000000'}
-                )
+                ),
+        html.Br(),
+        dcc.Markdown('Cluster Count'),
+        dbc.Input(id="cluster-count", type="number", value=2)
         
     ]
 )
@@ -209,6 +252,7 @@ content_second_row = dbc.Row(
     ]
 )
 
+
 content_third_row = dbc.Row(
     [
         dbc.Col(dcc.Graph(id='graph-benchmark'), md=12)
@@ -217,6 +261,13 @@ content_third_row = dbc.Row(
 
 
 content_fourth_row = dbc.Row(
+    [
+        dbc.Col(dcc.Graph(id='graph-cluster'), md=12)
+    ]
+)
+
+
+content_fifth_row = dbc.Row(
     [
         dbc.Col(
             dash_table.DataTable(
@@ -248,6 +299,8 @@ content = html.Div(
         content_third_row,
         html.Br(),
         content_fourth_row,
+        html.Br(),
+        content_fifth_row,
         dcc.Interval(
                 id='interval-component',
                 interval=30*1000, # in milliseconds 30sec update
@@ -369,7 +422,6 @@ def update_graph(selected_dropdown_value, start_date, end_date, n):
         template='plotly_white',
     ) 
 
-    
 
     return fig
 
@@ -388,7 +440,8 @@ def update_graph_bmrk(dropdown_bmak1_value, dropdown_bmak2_value, start_date, en
     fig = px.line(df, x="Date", y="CumReturn", color='Stock/ETF',
                  title="Cumulative Return"
                  )
-    fig.update_layout(height=600, template='plotly_dark')           
+    fig.update_layout(height=600, 
+            template='plotly_dark')           
     return fig
 
 
@@ -405,6 +458,67 @@ def update_pie_chart(my_dropdown):
             )
 
     return piechart """
+
+
+# clustering
+@app.callback(Output('graph-cluster', 'figure'),
+            [Input('my-dropdown', 'value'),
+            Input('my-date-picker-range', 'start_date'),
+            Input('my-date-picker-range', 'end_date'),
+            Input("cluster-count", "value")])
+def update_clustering(selected_dropdown_value, start_date, end_date, n_clusters):
+    # data preparation
+    df_cluster = get_df_cluster(l_cluster)
+    df = df_cluster.loc[:, ['AnnReturn', 'Volatility']]
+    
+    #K-means Clustering
+    # minimal input validation, make sure there's at least two clusters
+    km = KMeans(n_clusters=max(n_clusters, 2))
+    km.fit(df.values)
+    df_cluster["cluster"] = km.labels_
+    centers = km.cluster_centers_
+    
+    
+    #data points
+    data = [
+        go.Scatter(
+            x=df_cluster.loc[df_cluster.cluster == c, 'AnnReturn'],
+            y=df_cluster.loc[df_cluster.cluster == c, 'Volatility'],
+            mode="markers",
+            marker={"size": 10},
+            name="Cluster {}".format(c),
+            text=df_cluster.loc[df_cluster.cluster == c, 'Symbols']
+            
+        )
+        for c in range(n_clusters)
+    ]
+    
+    
+    # centers
+    data.append(
+        go.Scatter(
+            x=centers[:, 0],
+            y=centers[:, 1],
+            mode="markers",
+            marker={"color": "#000", "size": 8, "symbol": "diamond"},
+            name="Cluster centers",
+        )
+    )
+    
+    layout = {"xaxis": {"title": 'Annualized Return'}, "yaxis": {"title": 'Volatility'}}
+
+    fig = go.Figure(data=data, layout=layout)
+
+    fig.update_layout(height=600,
+        title = 'K-means Clustering',
+        template='seaborn')    
+    
+    return fig
+    
+
+
+
+
 
 
 # Table
